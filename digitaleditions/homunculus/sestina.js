27341a2498d4caa1244683bd2e1b980aeee63f5d
briarmoro <139605742+briarmoro@users.noreply.github.com>
2024-02-13 22:27:15 -0600

    Add files via upload

---
+++ digitaleditions/homunculus/sestina.js
@@ -0,0 +1,324 @@
+a_lines = ["The womb-wheel turning, flowing inertia through life-giving gears.", 
+"Like the earth rotating slowly through space, the mechanical brain lolled and spun between its wood-and-metal gears.",
+"The Earth turned in its steady routine, lost in a labyrinthine system of confusion, an orbular screw affixed into unfathomable but insignificant gears.",
+"His machinery turned, and churned, and grated into an unsteady awakening, a reanimating of mossy and age-worn gears.",
+"Time caught up, the race was tied, energy fell like flotsam into the whirlpool of the body’s worn-down gears.",
+"There is a soul that fills the machine, a person that lives beyond the moment that motion leaves the gears."];
+b_lines = ["The humble machine of birth-bearing, the body fabricating creation.",
+"Stars spun in flurries of excitement, incendiary snow burning the heavens and spitting fire onto creation.",
+"Nothing was permanent—everything seemed always on the verge of falling apart—he walked absentmindedly, daydreaming, through a ramshackle and half-formed creation.",
+"There was no motion—no flight—no creation.",
+"And there was nothing to sing to in all of creation.",
+"There is a thread that runs through everything in this odd creation."];
+c_lines = ["Eyes open, evincing the first flinch of life.",
+"Each time he blinked the world blinked with him, the firmament closing upon the earth and rising up again as he opened his eyes, spiraling sight back to eyes and the universe into renewed and reinvigorated life.",
+"Minutes blended to hours to years, time melting into a fluid amalgam of a life.",
+"Time fell down around him in waves of deconstruction, miniature apocalypses swirling in etheric eddies and pooling at the feet of his stagnant life.",
+"The sky cried out in songs of something-or-other—who could be sure what it meant?—and sent down rain like a humorous baptism of earthbound life.",
+"There is an unfathomable meaning in the strange chaos and indeterminacy of life."];
+d_lines = ["The life explodes—there is only truth, no hope of artifice.",
+"His being was born, but somehow uncreated—he could not help but feel, in his particular unsentient and unsentimental way, that he was not truly existent—merely an artifice.",
+"Falsehoods filled every moment of his day, paper sun and moon dangled in a firmamental mirror of his artifice.",
+"The ground beneath his feet pulsed like a neon light in love with its own artifice.",
+"There was no more running from the artifice.",
+"There is a truth that underlies and undermines all artifice."];
+e_lines = ["And the room stands still, and time halts as well (if only to be polite), and the infant takes its initial breath.",
+"The sky sighed deeply with each exhalation from the machine man; the moon clutched its chest each time he drew a breath.",
+"Nothing could steady him, nothing could draw him into the present, for the present was itself a fiction, terminally elusive, a vapor escaping from the lips and dissolving into a memory (already mostly forgotten) of one of innumerable gasps of breath.",
+"With each passing year, now, he struggled more and more just to take a breath.",
+"It was impossible to outpace the belabored weight of each failing breath.",
+"There is a harmony in our collective chorus of breath."];
+f_lines = ["And the beating goes on, and fills the room, and breaks like thunder in the eardrums deafened by the irrepressible sound, and the thunder is his heart.",
+"The machine had no life but the one that was given to it, a little wooden box carved crudely into the shape of a heart.",
+"But there was an omnipresent emptiness, a looming void in the center of his unbeating heart.",
+"A cinder of vagrant nostalgia landed on his forehead, burning a sigil of melancholy that floated gently down into his heart.",
+"A sweet painful song of sweetly painful love ran through his heart.",
+"There is a rhythm that gives music to the beating of our collective heart."];
+
+function changeWord_a_lines_1() {
+  let chosen_line = document.getElementsByClassName("a_line_1");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_a_lines_2() {
+  let chosen_line = document.getElementsByClassName("a_line_2");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_a_lines_3() {
+  let chosen_line = document.getElementsByClassName("a_line_3");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_a_lines_4() {
+  let chosen_line = document.getElementsByClassName("a_line_4");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_a_lines_5() {
+  let chosen_line = document.getElementsByClassName("a_line_5");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_a_lines_6() {
+  let chosen_line = document.getElementsByClassName("a_line_6");
+  let rndLine = a_lines[Math.floor(Math.random()*a_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_1() {
+  let chosen_line = document.getElementsByClassName("b_line_1");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_2() {
+  let chosen_line = document.getElementsByClassName("b_line_2");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_3() {
+  let chosen_line = document.getElementsByClassName("b_line_3");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_4() {
+  let chosen_line = document.getElementsByClassName("b_line_4");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_5() {
+  let chosen_line = document.getElementsByClassName("b_line_5");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_bLines_6() {
+  let chosen_line = document.getElementsByClassName("b_line_6");
+  let rndLine = b_lines[Math.floor(Math.random()*b_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_1() {
+  let chosen_line = document.getElementsByClassName("c_line_1");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_2() {
+  let chosen_line = document.getElementsByClassName("c_line_2");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_3() {
+  let chosen_line = document.getElementsByClassName("c_line_3");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_4() {
+  let chosen_line = document.getElementsByClassName("c_line_4");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_5() {
+  let chosen_line = document.getElementsByClassName("c_line_5");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_cLines_6() {
+  let chosen_line = document.getElementsByClassName("c_line_6");
+  let rndLine = c_lines[Math.floor(Math.random()*c_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_1() {
+  let chosen_line = document.getElementsByClassName("d_line_1");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_2() {
+  let chosen_line = document.getElementsByClassName("d_line_2");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_3() {
+  let chosen_line = document.getElementsByClassName("d_line_3");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_4() {
+  let chosen_line = document.getElementsByClassName("d_line_4");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_5() {
+  let chosen_line = document.getElementsByClassName("d_line_5");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_dLines_6() {
+  let chosen_line = document.getElementsByClassName("d_line_6");
+  let rndLine = d_lines[Math.floor(Math.random()*d_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_1() {
+  let chosen_line = document.getElementsByClassName("e_line_1");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_2() {
+  let chosen_line = document.getElementsByClassName("e_line_2");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_3() {
+  let chosen_line = document.getElementsByClassName("e_line_3");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_4() {
+  let chosen_line = document.getElementsByClassName("e_line_4");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_5() {
+  let chosen_line = document.getElementsByClassName("e_line_5");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_eLines_6() {
+  let chosen_line = document.getElementsByClassName("e_line_6");
+  let rndLine = e_lines[Math.floor(Math.random()*e_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_1() {
+  let chosen_line = document.getElementsByClassName("f_line_1");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_2() {
+  let chosen_line = document.getElementsByClassName("f_line_2");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_3() {
+  let chosen_line = document.getElementsByClassName("f_line_3");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_4() {
+  let chosen_line = document.getElementsByClassName("f_line_4");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_5() {
+  let chosen_line = document.getElementsByClassName("f_line_5");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
+
+function changeWord_fLines_6() {
+  let chosen_line = document.getElementsByClassName("f_line_6");
+  let rndLine = f_lines[Math.floor(Math.random()*f_lines.length)];
+  for (var i = 0; i < chosen_line.length; i++) {
+    chosen_line[i].innerHTML = rndLine;
+  }
+}
